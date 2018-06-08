29d10f3ba6523db852200b45fe131f8ae8e114e1
MIYAMA8810 <zenonandalice@gmail.com>
2018-06-08 15:24:51 +0900

    メソッドの課題を提出します

---
+++ CampChallenge_java/CampChallenge_java/011.メソッド/Challenge11.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.challenge.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Challenge11 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Challenge11</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            // out.println("<h1>Servlet Challenge11 at " + request.getContextPath() + "</h1>");
+            
+            // ユーザー定義メソッドの使用
+            intro(out);
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+    
+    // ユーザー定義メソッド
+    public void intro(PrintWriter pw) {
+        
+        // 表示処理
+        pw.println("名前は深山颯斗です。<br>");
+        pw.println("1995年7月25日生まれです。<br>");
+        pw.println("趣味でルアーフィッシングをしています。よろしくお願いします。<br>");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ CampChallenge_java/CampChallenge_java/011.メソッド/Challenge11ArgsReturn2Servlet.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.challenge.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Challenge11ArgsReturn2Servlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            
+            // ユーザー定義メソッドから情報を受け取る配列の宣言と、呼び出して情報を代入
+            String[] prof = prof("0125");
+            
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Challenge11ArgsReturn2Servlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            //out.println("<h1>Servlet Challenge11ArgsReturn2Servlet at " + request.getContextPath() + "</h1>");
+                          
+            // 受け取った値の表示
+            for(int i = 1; i < 3; i++) {
+                
+                if(prof[i] == null) {
+                    continue;
+                }
+                out.print(prof[i] +"<br>");
+            }
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+     // ユーザー定義メソッド
+    public String[] prof(String id) {
+        
+        // ユーザー情報を生成、格納
+        String[] user1 = {"0123", "1995/07/25", "東京都"};
+        String[] user2 = {"0124", "1993/05/22", "群馬県"};
+        String[] user3 = {"0125", "1995/03/04", null};
+        
+        // 戻り値をしまう配列の生成
+        String[] ans = new String[3];
+        
+        // 受け取った引数と同じ情報が入っている配列を探す
+        if(user1[0].equals(id)) {
+            
+            ans = user1;
+        } else if(user2[0].equals(id)) {
+            
+            ans = user2;
+        } else if(user3[0].equals(id)) {
+            
+            ans = user3;
+        }
+        
+        // 配列を返す 
+        return ans;
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
